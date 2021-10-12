f9c41fff9bf2818548e908b59cd30a5e3cbbb657
Dominic <69826136+moddingdom@users.noreply.github.com>
2021-10-11 21:21:30 -0500

    Add files via upload

---
+++ windowsoptimizer.cmd
@@ -0,0 +1,146 @@
+@echo off
+title WindowsOptimizer by domm#0072
+echo This will delete all Temp Files, Disable Windows Defender & Disables Useless Windows Services.
+echo Press Ctrl+C to exit or
+pause
+
+del %TEMP% /F /S /Q
+rmdir %TEMP% /S /Q
+del C:\Windows\Temp /F /S /Q
+rmdir C:\Windows\Temp /S /Q
+
+cd C:\Windows\system32
+
+sc stop BITS
+sc stop wuauserv
+
+sc config BITS start= disabled
+sc config wuauserv start= disabled
+
+sc config MapsBroker start=disabled
+sc config WSearch start=disabled
+
+sc config SSDPSRV start=disabled
+
+sc config lfsvc start=disabled
+
+sc config AXInstSV start=disabled
+
+sc config AJRouter start=disabled
+
+sc config AppReadiness start=disabled
+
+sc config HomeGroupListener start=disabled
+
+sc config HomeGroupProvider start=disabled
+
+sc config SharedAccess start=disabled
+
+sc config lltdsvc start=disabled
+
+sc config wlidsvc start=disabled
+
+sc config SmsRouter start=disabled
+
+sc config NcdAutoSetup start=disabled
+
+sc config PNRPsvc start=disabled
+
+sc config p2psvc start=disabled
+
+sc config p2pimsvc start=disabled
+
+sc config PNRPAutoReg start=disabled
+
+sc config WalletService start=disabled
+
+sc config WMPNetworkSvc start=disabled
+
+sc config icssvc start=disabled
+
+sc config XblAuthManager start=disabled
+
+sc config XblGameSave start=disabled
+
+sc config XboxNetApiSvc start=disabled
+
+sc config DmEnrollmentSvc start=disabled
+
+sc config RetailDemo start=disabled
+
+C:\Program Files\Windows Defender\MpCmdRun.exe" -RemoveDefinitions -All Set-MpPreference -DisableIOAVProtection $true
+
+powershell.exe -command "Set-MpPreference -DisableRealtimeMonitoring $true"
+
+powershell.exe -command "Set-MpPreference -DisableBehaviorMonitoring $true"
+
+powershell.exe -command "Set-MpPreference -DisableBlockAtFirstSeen $true"
+
+powershell.exe -command "Set-MpPreference -DisableIOAVProtection $true"
+
+powershell.exe -command "Set-MpPreference -DisablePrivacyMode $true"
+
+powershell.exe -command "Set-MpPreference -SignatureDisableUpdateOnStartupWithoutEngine $true"
+powershell.exe -command "Set-MpPreference -DisableArchiveScanning $true"
+
+powershell.exe -command "Set-MpPreference -DisableIntrusionPreventionSystem $true"
+powershell.exe -command "Set-MpPreference -DisableScriptScanning $true"
+powershell.exe -command "Set-MpPreference -SubmitSamplesConsent 2"
+
+powershell.exe -command "Set-MpPreference -MAPSReporting 0"
+powershell.exe -command "Set-MpPreference -HighThreatDefaultAction 6 -Force"
+powershell.exe -command "Set-MpPreference -ModerateThreatDefaultAction 6"
+      
+powershell.exe -command "Set-MpPreference -LowThreatDefaultAction 6"
+
+powershell.exe -command "Set-MpPreference -SevereThreatDefaultAction 6"
+
+powershell.exe -command "Reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f"
+powershell.exe -command "REG ADD “hklm\software\policies\microsoft\windows defender” /v DisableAntiSpyware /t REG_DWORD /d 1 /f"
+
+powershell.exe -command "netsh advfirewall set allprofiles state off"
+
+reg delete "HKLM\Software\Policies\Microsoft\Windows Defender" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiVirus" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\MpEngine" /v "MpEnablePus" /t REG_DWORD /d "0" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableIOAVProtection" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableOnAccessProtection" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableScanOnRealtimeEnable" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Reporting" /v "DisableEnhancedNotifications" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\SpyNet" /v "DisableBlockAtFirstSeen" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\SpyNet" /v "SpynetReporting" /t REG_DWORD /d "0" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\SpyNet" /v "SubmitSamplesConsent" /t REG_DWORD /d "2" /f
+cls
+rem 0 - Disable Logging
+reg add "HKLM\System\CurrentControlSet\Control\WMI\Autologger\DefenderApiLogger" /v "Start" /t REG_DWORD /d "0" /f
+reg add "HKLM\System\CurrentControlSet\Control\WMI\Autologger\DefenderAuditLogger" /v "Start" /t REG_DWORD /d "0" /f
+cls
+rem Disable WD Tasks
+schtasks /Change /TN "Microsoft\Windows\ExploitGuard\ExploitGuard MDM policy Refresh" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable
+cls 
+rem Disable WD systray icon
+reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run" /v "SecurityHealth" /f
+reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v "SecurityHealth" /f
+cls
+rem Remove WD context menu
+reg delete "HKCR\*\shellex\ContextMenuHandlers\EPP" /f
+reg delete "HKCR\Directory\shellex\ContextMenuHandlers\EPP" /f
+reg delete "HKCR\Drive\shellex\ContextMenuHandlers\EPP" /f
+cls
+rem Disable WD services
+reg add "HKLM\System\CurrentControlSet\Services\WdBoot" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\System\CurrentControlSet\Services\WdFilter" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\System\CurrentControlSet\Services\WdNisDrv" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\System\CurrentControlSet\Services\WdNisSvc" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\System\CurrentControlSet\Services\WinDefend" /v "Start" /t REG_DWORD /d "4" /f
+
+echo Windows Optimized!
+echo Exit:
+pause
